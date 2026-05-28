24dde3a02e916e26e1f65db2a8e471152b7ebb7a
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2026-05-28 09:37:43 +0530

    implemented encapsulation

--- Learn/OOPS.java
+++ Learn/OOPS.java
@@ -1,26 +1,4 @@
-public class OOPS {
-
-    static void studentOOPS() {
-        Student A = new Student(); // calls the default constructor.
-        // set attributes line by line for default constr. Default attributes = null
-        A.id = 1234;
-        A.name = "Sam";
-        A.age = 17;
-        A.section = 'C';
-        A.study();
-
-        Student B = new Student(9876, "Peter", 18, 'B'); // send attributes and parameters.
-        B.study();
-
-        Student C = new Student(B); // copies B's set attributes to C
-        C.study();
-    }
-    
-    public static void main(String[] args) {
-        studentOOPS();
-    }
-}
-
+import java.util.Scanner;
 
 class Student {
     // Attributes
@@ -34,6 +12,9 @@ class Student {
         System.out.println(name + " is studying.");
     }
 
+    private void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
     // Constructors -> public ClassName() | They are called whenever the object/instance of the class is created.
 
     // Default constructor
@@ -44,7 +25,7 @@ class Student {
     // Parameterized constructor
     public Student(int sid, String name, int age, char section) {
         // note: this.(attribute) -> refers to class attributes; sid, name etc -> parameters passed
-        this.id = sid;
+        id = sid; // no need to use "this" keyword since the parameter name and the attribute names are different.
         this.name = name;
         this.age = age;
         this.section = section;
@@ -62,4 +43,96 @@ class Student {
         System.out.println("Copy constructor called.");
     }
 
+}
+
+// Implementing Perfect Encapsulation (putting restrictions on data accessibility | hiding data)
+class Teacher {
+    private int id; // can only access private attributes within this class. Can't access externally or via a derived class.
+    private String name;
+    private int age;
+    private String[] subjects;
+
+    public Teacher(int id, String name, int age, String[] subjects) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.subjects = subjects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Adding an authentication/security layer for getter & setter methods.
+    public int getId(boolean isAuthenticated) {
+        if (isAuthenticated)
+            return id;
+        else {
+            System.out.println("User is not authenticated");
+            return 0;
+        }
+    }
+
+    public void setId(boolean isAuthenticated, int id) {
+        if (isAuthenticated)
+            this.id = id;
+        else
+            System.out.println("User is not authenticated.");
+    }
+}
+
+public class OOPS {
+
+    static String username = "user1234";
+    static int password = 1234;
+    static boolean isAuthenticated = false;
+
+    static void authenticate() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username");
+        String _username = sc.nextLine();
+        System.out.println("Enter password");
+        int _password = sc.nextInt();
+        sc.close();
+
+        if (_username.equalsIgnoreCase(username) && (_password == password)) {
+            isAuthenticated = true;
+            System.out.println("Authentication successful");
+        }
+        else
+            System.out.println("Authentication failed.");
+    }
+
+    static void studentOOPS() {
+        Student A = new Student(); // calls the default constructor.
+        // set attributes line by line for default constr. Default attributes = null
+        A.id = 1234;
+        A.name = "Sam";
+        A.age = 17;
+        A.section = 'C';
+        A.study();
+
+        Student B = new Student(9876, "Peter", 18, 'B'); // send attributes and parameters.
+        B.study();
+
+        Student C = new Student(B); // copies B's set attributes to C
+        C.study();
+    }
+
+    static void teacherOOPS() {
+        Teacher T = new Teacher(134, "Frank", 41, new String[]{"Math", "Physics", "Chemistry"});
+        System.out.println(T.getId(isAuthenticated));
+        System.out.println(T.getName());
+        T.setName("Henry");
+        System.out.println(T.getName());
+    }
+    
+    public static void main(String[] args) {
+        authenticate();
+        teacherOOPS();
+    }
 }
