ed0c8bb3b75fe00196da571c081a477de56c17be
Rudransh <rudranshsrivastava2525@gmail.com>
2026-06-22 15:18:57 +0530

    learnt about hashset

---
+++ Learn/CollectionFramework/SetMain.java
@@ -0,0 +1,31 @@
+package CollectionFramework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+- Set contains only unique elements. Elemens can't be repeated in a set.
+- Each element that is added has a hashcode that decides where the element will be stored in the 
+datastructure. Set doesn't retain the order of addition of elements to the datastruct.
+*/
+
+public class SetMain {
+    static void hashSetFunc() {
+        Set<Integer> st1 = new HashSet<>();
+        for (int i=10; i<60; i+=10) {
+            st1.add(i);
+        }
+        Set<Integer> st2 = new HashSet<>();
+        st2.add(10);
+        st2.add(20);
+
+        st1.containsAll(st2); // true, since 10, 20 is present in s1.
+        st2.containsAll(st1); // false, since 30, 40, 50 is not present in s2. 
+
+        // intersection of sets
+        st1.retainAll(st2); // 10, 20 since only those elements are common in both
+    }
+    public static void main(String[] args) {
+        hashSetFunc();
+    }
+}
