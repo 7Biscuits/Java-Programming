abbeec643064ba34f2517400109d00bc0b9cc842
Rudransh <rudranshsrivastava2525@gmail.com>
2026-07-27 04:03:15 +0530

    minor updates

--- Learn/Bit/Main.java
+++ Learn/Bit/Main.java
@@ -8,7 +8,8 @@ public class Main {
      * logical operators for bits:
      * bitwise AND: n & m
      * bitwise OR : n | m
-     * bitwise XOR: n ^ m; bit is 1 if the corresponding bits are different: 1100 ^ 1010 = 0110
+     * bitwise XOR: n ^ m; bit is 1 if the corresponding bits are different: 1100 ^
+     * 1010 = 0110
      * bitwise NOT: ~n: flips each bit: ~(1001) = 0110
      */
 
@@ -16,7 +17,8 @@ public class Main {
         /*
          * logic: n & 1 gives the right most bit of the number (if bit = 0 then 0 & 1
          * gives 0; 1 & 1 gives 1).
-         * An even integer's right most bit will always be 0 and odd integer's will be 1.
+         * An even integer's right most bit will always be 0 and odd integer's will be
+         * 1.
          * example:
          * 2 = 0000 0010
          * 3 = 0000 0011
@@ -63,21 +65,25 @@ public class Main {
 
     // note: the "1" bit in binary is called "set bit"
 
-    // this function checks if a number is a power of two i.e 2^n using a bruteforce method.
-    /* logic: if a number is power of 2, it will have only one set-bit/"1" in its binary form.
-        2^0: 1 in binary = 0000 0001
-        2^1: 2 in binary = 0000 0010
-        2^2: 4 in binary = 0000 0100
-
-        So, if we count the no. of set bits in a number we can find out if it can be a power of 2.
-        */
+    // this function checks if a number is a power of two i.e 2^n using a bruteforce
+    // method.
+    /*
+     * logic: if a number is power of 2, it will have only one set-bit/"1" in its
+     * binary form.
+     * 2^0: 1 in binary = 0000 0001
+     * 2^1: 2 in binary = 0000 0010
+     * 2^2: 4 in binary = 0000 0100
+     * 
+     * So, if we count the no. of set bits in a number we can find out if it can be
+     * a power of 2.
+     */
 
     static void checkPowerOf2Bruteforce(int n) {
         int m = n;
         int count = 0;
         int pow = -1;
         while (n != 0) {
-            if ((n&1) != 0) {
+            if ((n & 1) != 0) {
                 count++;
             }
             pow++;
@@ -86,51 +92,53 @@ public class Main {
 
         if (count == 1) {
             System.out.println(m + " is a power of 2.");
-            System.out.println(m + " = 2^"+pow);
-        } 
+            System.out.println(m + " = 2^" + pow);
+        }
     }
 
     /*
-    * non-bruteforce method to check if n is a power of 2.
-    * let n = 8.
-    * 8 = 0000 1000
-    * (n-1): 7 = 0000 0111
-    * 8 & 7: 1000 & 0111 = 0000 0000
-    * hence, if n & (n-1) = 0 then n is power of 2
-    */ 
+     * non-bruteforce method to check if n is a power of 2.
+     * let n = 8.
+     * 8 = 0000 1000
+     * (n-1): 7 = 0000 0111
+     * 8 & 7: 1000 & 0111 = 0000 0000
+     * hence, if n & (n-1) = 0 then n is power of 2
+     */
     static void checkPowerOf2(int n) {
-        if ((n & (n-1)) == 0) {
+        if ((n & (n - 1)) == 0) {
             System.out.println(n + " is a power of 2.");
         }
     }
 
-    /* Logic:
-    Swapping a and b i.e a will be = b and b will be equal to a.
-    Property of XOR: n ^ n = 0 | n ^ 0 = n
-    let a = 2, b = 3
-    a = a^b = 2^3
-    b = a^b = (2^3)^3 = 2^(3^3) = 2
-    a = a^b = (2^3)^2 = 3^(2^2) = 3
-
-    a and b are swapped.
-    */
+    /*
+     * Logic:
+     * Swapping a and b i.e a will be = b and b will be equal to a.
+     * Property of XOR: n ^ n = 0 | n ^ 0 = n
+     * let a = 2, b = 3
+     * a = a^b = 2^3
+     * b = a^b = (2^3)^3 = 2^(3^3) = 2
+     * a = a^b = (2^3)^2 = 3^(2^2) = 3
+     * 
+     * a and b are swapped.
+     */
     static void swapTwoNumberUsingXOR(int a, int b) {
         System.out.println(a + ", " + b);
         // swap logic
-        a = a^b;
-        b = a^b;
-        a = a^b;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
         System.out.println(a + ", " + b);
     }
 
-    // question: find unique element from a list where all other elements appear twice.
+    // question: find unique element from a list where all other elements appear
+    // twice.
     // bruteforce method, O(n^2)
     static void findUniqueElementBruteforce() {
         int[] arr = { 1, 2, 3, 6, 3, 2, 1 };
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             int target = arr[i];
             int count = 0;
-            for (int j=0; j<arr.length; j++) {
+            for (int j = 0; j < arr.length; j++) {
                 if (target == arr[j]) {
                     count++;
                 }
@@ -141,29 +149,53 @@ public class Main {
             }
         }
     }
-    /* logic:
-    1^2^3^6^3^2^1 = (1^1)^(2^2)^(3^3)^6 = 6. Since n^n = 0
-    time complexity: O(n)
+
+    /*
+     * logic:
+     * 1^2^3^6^3^2^1 = (1^1)^(2^2)^(3^3)^6 = 6. Since n^n = 0
+     * time complexity: O(n)
      */
     static void findUniqueElement() {
         int[] arr = { 1, 2, 3, 6, 3, 2, 1 };
 
         int unique = arr[0];
-        for (int i = 1; i<arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             unique = unique ^ arr[i];
         }
         System.out.println(unique + " is the unique element.");
     }
 
-    /* logic: n & (n-1) removes last set bit.
-    example: n = 7
-    7 = 0111; n-1 = 6 = 0110
-    last set bit in 7: 011[1]
-    7 & 6 = 0111 & 0110 = 0110
-    the last set bit was removed.
+    static void findTwoUniqueElements() {
+        int[] arr = { 1, 2, 3, 6, 7, 3, 2, 1 };
+    }
+
+    /*
+     * logic: n & (n-1) removes last set bit.
+     * example: n = 7
+     * 7 = 0111; n-1 = 6 = 0110
+     * last set bit in 7: 011[1]
+     * 7 & 6 = 0111 & 0110 = 0110
+     * the last set bit was removed.
      */
     static void removeLastSetBit(int n) {
-        int m = n&(n-1); // remove the last setbit
+        int m = n & (n - 1); // remove the last setbit
+        System.out.println(m);
+    }
+
+    /*
+     * note: steps to find a negative of the number
+     * use the bitwise NOT (~) operator on the number.
+     * perform a binary addition of 1 on the flipped number (i.e add 1 to its binary form)
+     * example: n = 9 = 0000 1001
+     * ~n = 1111 0110
+     * ~n+1:
+     *   1111 0110
+     * + 0000 0001
+     * = 1111 0111 = -9
+     */
+    static void findNegativeOfNumber(int n) {
+        int m = ~n;
+        m += 1;
         System.out.println(m);
     }
 
@@ -173,5 +205,6 @@ public class Main {
         // swapTwoNumberUsingXOR(2, 3);
         // findUniqueElement();
         // removeLastSetBit(10);
+        findNegativeOfNumber(9);
     }
 }
