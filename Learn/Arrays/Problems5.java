0fff8d0e14fa3812d01bef94be7a1af731265ed1
Rudransh <rudranshsrivastava2525@gmail.com>
2026-09-05 18:10:46 +0530

    solved column sum problem

--- Learn/Arrays/Problems5.java
+++ Learn/Arrays/Problems5.java
@@ -12,22 +12,24 @@ public class Problems5 {
     static int maximumSubarray(int[] arr) {
         int maxSum = Integer.MIN_VALUE;
         int sum = 0;
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
-            if (sum > maxSum) maxSum = sum;
-            if (sum < 0) sum = 0;
+            if (sum > maxSum)
+                maxSum = sum;
+            if (sum < 0)
+                sum = 0;
         }
 
         return maxSum;
     }
 
-    // [[1, 2, 3], [4, 5, 6], [7, 8, 9]] -> 6 + 16 + 24
-    // [[1], [2, 2], [3, 3, 3]]
+    // [[1, 2, 3], [4, 5, 6], [7, 8, 9]] -> [6, 16, 24]
+    // [[1], [2, 2], [3, 3, 3]] -> [1, 4, 9]
     static List<Integer> rowSums(int[][] arr) {
         List<Integer> list = new ArrayList<>();
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             int sum = 0;
-            for (int j=0; j<arr[i].length; j++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 sum += arr[i][j];
             }
             list.add(sum);
@@ -35,8 +37,27 @@ public class Problems5 {
         return list;
     }
 
+    /*
+     * Matrix: [[1, 2, 3],
+     *          [4, 5, 6],
+     *          [7, 8, 9]]
+     * 
+     * Result: [12, 15, 18]
+     */
+    static List<Integer> columnSums(int[][] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[j][i];
+            }
+            list.add(sum);
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
-        int max = maximumSubarray(new int[] {-2,1,-3,4,-1,2,1,-5,4});
-        System.out.println(max);
+        List<Integer> list = columnSums(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+        System.out.println(list);
     }
 }
